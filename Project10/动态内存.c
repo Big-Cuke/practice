ee735421040c6f8c24b73eeba3d43445e5178722
Big-Cuke <1171156962@qq.com>
2021-01-05 22:26:17 +0800

    1

---
+++ Project10/动态内存.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main()
+//{
+//	/*int num = 0;
+//	scanf("%d\n", &num);
+//	int arr[num] = {0};*/       //异想天开，明显不可能
+//	
+//	/*int num = 0;
+//	int* ptr = NULL;
+//	ptr = (int*)malloc(num*sizeof(int));
+//	if (NULL != ptr)
+//	{
+//		int i = 0;
+//		for (i=0; i<num; i++)
+//		{
+//			*(ptr + i) = 0;
+//		}
+//	}
+//	free(ptr);
+//	ptr = NULL;*/
+//
+//	int num = 0;
+//	//初始化动态内存
+//	int* ptr = NULL;
+//	//开辟空间
+//	ptr = (int*)malloc(num * sizeof(int));
+//	//判断是否开辟成功
+//	if (NULL != ptr)
+//	{
+//		int i = 0;
+//		for (i=0; i<num; i++)
+//		{
+//			*(ptr + i) = 0;
+//		}
+//	}
+//	//释放动态内存,并且把值制位NULL
+//	free(ptr);
+//	ptr = NULL;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int* p = calloc(10, sizeof(int)); //默认初始化为00
+//	if (NULL != p)
+//	{}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
